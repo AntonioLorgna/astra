34f71d87f543b9860ee7e35a6f6f2dbe41c86d67
AntonioLorgna <tonny30@disroot.org>
2023-02-23 17:11:07 +0400

    clear api.core from crud functionality

--- astra/api/core.py
+++ astra/api/core.py
@@ -16,59 +16,6 @@ from astra.core.schema import TaskInfo
 aiohttp_typedefs.DEFAULT_JSON_ENCODER = orjson.dumps
 aiohttp_typedefs.DEFAULT_JSON_DECODER = orjson.loads
 
-def get_user(id: str):
-    with Session(engine) as session:
-        statement = select(models.User) \
-            .where(models.User.id == id)
-        return session.exec(statement).first()
-
-def add_user(role: int = 0, limit_seconds: int = 0):
-    with Session(engine) as session:
-        user = models.User(
-            role=role, 
-            bank_seconds=limit_seconds)
-        
-        session.add(user)
-        session.commit()
-        return user
-
-def get_account(tg_id: str):
-    with Session(engine) as session:
-        statement = select(models.ServiceAccount) \
-            .where(models.ServiceAccount.service_id == tg_id,
-                   models.ServiceAccount.service_name == 'telegram')
-        return session.exec(statement).first()
-
-def get_user_tg(tg_id: str, session: Session):
-    statement = select(models.User, models.ServiceAccount) \
-        .where(models.User.id == models.ServiceAccount.user_id,
-                models.ServiceAccount.service_id == tg_id,
-                models.ServiceAccount.service_name == 'telegram')
-    t = session.exec(statement).first()
-    if t is None: return (None, None)
-    return t
-
-def add_user_tg(tg_id: str, session: Session, role: int = 0, limit_seconds: int = 0):
-    session.begin_nested()
-    session.execute('LOCK TABLE "user" IN ACCESS EXCLUSIVE MODE;')
-    user = models.User(
-        role=role, 
-        bank_seconds=limit_seconds)
-    
-    session.add(user)
-    session.commit()
-    session.begin_nested()
-    session.execute('LOCK TABLE "service_account" IN ACCESS EXCLUSIVE MODE;')
-    account = models.ServiceAccount(
-        service_id=tg_id,
-        service_name='telegram',
-        user=user
-    )
-    session.add(account)
-    session.commit()
-    session.commit()
-    return (user, account)
-
 async def add_task(task_init: models.TaskBase):
     payload = task_init.json()
     headers = {
@@ -85,10 +32,6 @@ async def add_task(task_init: models.TaskBase):
             
             logger.error(f"Can not add new task: '{await response.content.read()}' ({response.status})")
     return None
-
-def get_task(job_id: str, session: Session):
-    return session.get(models.Task, id)
-
 
 async def result_exist(info: TaskInfo, bot, user_id):
     result = schema.TranscribeResult(**orjson.loads(info.result))
